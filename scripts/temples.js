dcdcceb68a42b5c3ed787bd72c25b4a110ec33e2
ncalderonmita <nicomatiu527@gmail.com>
2026-09-12 15:49:39 -0400

    hamburger

--- scripts/temples.js
+++ scripts/temples.js
@@ -1,5 +1,13 @@
 const year = document.querySelector("#currentyear");
 const today = new Date();
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
 year.innerHTML = `<span class="highlight">${today.getFullYear()}</span>`;
 
 document.getElementById("lastModified").textContent = document.lastModified;
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
